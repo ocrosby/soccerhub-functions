03ffbfef6b763fff7a0956d5aab4bcb3073bf565
omar.crosby <omar.crosby@gmail.com>
2019-05-12 02:05:53 -0400

    Refactoring the function to utilize the new extraction code.

--- DevelopmentAcademyClubs/index.js
+++ DevelopmentAcademyClubs/index.js
@@ -1,16 +1,40 @@
+'use strict';
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+const cheerioTableparser = require('cheerio-tableparser');
+
+const ClubExtractor = require('./club').ClubExtractor;
+
+const allClubsURL = 'http://www.ussoccerda.com/all-clubs';
+
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
+    console.log('Retrieving clubs ...');
+
+    let response = await axios.get(allClubsURL);
+
+    if (response.status === 200) {
+        let $ = cheerio.load(response.data);
+
+        cheerioTableparser($);
+        let data = $("table#club_directory_list").parsetable(true, true, true);
+        let clubs = [];
+
+        for(let i = 2 ; i < data[1].length ; i++) {
+            clubs.push(ClubExtractor.getClub(data, i));
+        } // end for
+
         context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            status: 200,
+            body: { count: clubs.length, data: clubs }
         };
-    }
-    else {
+    } else {
         context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            status: 500,
+            body: `The response status code was ${response.status}!`
         };
     }
 };
